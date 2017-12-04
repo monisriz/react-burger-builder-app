0b3ffd3ef1125409bc0c687b8f37f553c506f719
Monis Rizvi <monisriz@gmail.com>
2017-12-04 11:00:41 -0600

    burger builder app

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,4 +1,5 @@
 import React, {Fragment} from 'react';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
@@ -15,7 +16,14 @@ const orderSummary = (props) => {
       <ul>
         {ingredientSummary}
       </ul>
-      <p>Continue to order</p>
+      <p>Total Price: <strong>${props.price.toFixed(2)}</strong></p>
+      <p>Continue to order?</p>
+      <Button
+        btnType='Danger'
+        clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button
+        btnType='Success'
+        clicked={props.purchaseContinued}>CONTINUE</Button>
     </Fragment>
   );
 };
