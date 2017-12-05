496cff4da74e5cb7f8ff65d599ae8091647fdc90
Monis Rizvi <monisriz@gmail.com>
2017-12-05 09:02:38 -0600

    burger builder app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,13 @@
 import React, {Fragment} from 'react';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Fragment>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <div>Backdrop</div>
+    <SideDrawer />
+    <Toolbar />
     <main className={classes.Content}>
       {props.children}
     </main>
